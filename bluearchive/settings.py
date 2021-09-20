4781ada350d5cb193e961659fd015c687bec9bf5
d1tn <d1taniguchi@gmail.com>
2021-09-21 01:30:06 +0900

    setting編集

--- bluearchive/settings.py
+++ bluearchive/settings.py
@@ -121,7 +121,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = (os.path.join(BASE_DIR, 'static'),
+     '/var/www/static/',
+)
 
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, "static"),
