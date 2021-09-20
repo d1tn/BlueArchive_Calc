53e9de419bc13d19de85ee5a07fc1e91ae746f4f
d1tn <d1taniguchi@gmail.com>
2021-09-21 02:11:23 +0900

    setting編集

--- bluearchive/settings.py
+++ bluearchive/settings.py
@@ -121,14 +121,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = (os.path.join(BASE_DIR, 'static'),
-     '/var/www/static/',
-)
-
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, "static"),
-#     '/var/www/static/',
+# STATIC_ROOT = (os.path.join(BASE_DIR, 'static'),
+#      '/var/www/static/',
 # )
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
