e3240a90144f149b844742934a25ad49e5f9ef53
d1tn <d1taniguchi@gmail.com>
2021-09-23 15:52:23 +0900

    setting修正

--- bluearchive/settings.py
+++ bluearchive/settings.py
@@ -117,10 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 # 本番環境の静的ファイル格納ディレクトリ
-STATIC_ROOT = (
-os.path.join(BASE_DIR, 'static'),
-'/var/www/static/',
-)
+# STATIC_ROOT = '/var/www/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
