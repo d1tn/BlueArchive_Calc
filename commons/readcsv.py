ff803814362c680548105816fc8eb29b178a335b
d1tn <d1taniguchi@gmail.com>
2021-09-30 03:13:54 +0900

    csv修正

--- commons/readcsv.py
+++ commons/readcsv.py
@@ -13,6 +13,8 @@ stuData = pd.DataFrame(pd.read_csv(csvFolder + "Main/00_Students_Data.csv", enco
 stuOP = pd.DataFrame(pd.read_csv(csvFolder + "Main/11_Students_OP.csv", encoding="utf-8"))
 stuOP = stuOP.fillna(stuOP.mean(numeric_only=True)+1).astype('int',errors='ignore')
 
+display(stuOP)
+
 #その他データ
 schl = pd.DataFrame(pd.read_csv(csvFolder + "Main/01_School.csv", encoding="utf-8"))
 clb = pd.DataFrame(pd.read_csv(csvFolder + "Main/02_Club.csv", encoding="utf-8"))
