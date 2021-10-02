255ba5ab8c317dc6891ec593b8f4aa158e2b7464
d1tn <d1taniguchi@gmail.com>
2021-10-03 04:02:44 +0900

    urls修正

--- calc/urls.py
+++ calc/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('', views.charchoise, name='charchoise'),
     path('input', views.input, name='input'),
     path('calc', views.calc, name='calc'),
-    path('del', views.delSession, name='del'),
     path('howto', views.howto, name='howto'),
     path('about', views.about, name='about'),
     path('privacypolicy', views.privacypolicy, name='privacypolicy'),
