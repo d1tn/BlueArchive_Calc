d30248552361924bfd38cef1338b9e892d68e192
d1tn <d1taniguchi@gmail.com>
2021-10-05 22:27:08 +0900

    urls.py修正

--- calc/urls.py
+++ calc/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 from . import views
 
 app = 'first_app'
+
 urlpatterns = [
     path('', views.charchoise, name='charchoise'),
     path('input', views.input, name='input'),
     path('result', views.result, name='result'),
     path('saveConfirm', views.saveConfirm, name='saveConfirm'),
-    path('saved', views.saved, name='saved'),    path('loadConfirm', views.loadConfirm, name='loadConfirm'),
-    path('loaded', views.loaded, name='loaded'),
+    path('saved', views.saved, name='saved'),    path('loadData', views.loadData, name='loadData'),
+    # path('loaded', views.loaded, name='loaded'),
     path('howto', views.howto, name='howto'),
     path('about', views.about, name='about'),
     path('privacypolicy', views.privacypolicy, name='privacypolicy'),
